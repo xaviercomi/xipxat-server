4175443d1bcaa25e15382ac5f88cec63cab3db0e
xcomi <xaviercomi@gmail.com>
2023-07-22 18:46:00 +0200

    database connection working

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 
 module.exports = {
 
-    DB: process.env.MONGODB || 'mongodb+srv://xaviercomi:9tXJqmEEM6k*^woF@wn4@xipxat.uil5m2u.mongodb.net/xipxat?retryWrites=true&w=majority',
+    DB: process.env.ATLAS_URI || 'mongodb+srv://xaviercomi:9tXJqmEEM6k%2A%5EwoF%40wn4@xipxat.uil5m2u.mongodb.net/?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
-
 }
--- database.js
+++ database.js
@@ -2,6 +2,7 @@
 const mongoose = require('mongoose');
 const { DB } = require('./config');
 mongoose.set("strictQuery", false);
+console.log(DB);
 mongoose.connect(DB)
     .then(db => console.log('✔️  database ready!'))
     .catch(err => console.log(`❌ database NOT ready`))
