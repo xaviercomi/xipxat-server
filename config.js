87e7d2dad650d15fbb674e93ac792b1f4b3ff49a
xcomi <xaviercomi@gmail.com>
2023-07-22 14:20:10 +0200

    new connection string

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 
-    DB: process.env.MONGODB || 'mongodb+srv://xaviercomi:9tXJqmEEM6k*^woF@wn4@xipxat.uil5m2u.mongodb.net/?retryWrites=true&w=majority',
+    DB: process.env.MONGODB || 'mongodb+srv://xaviercomi:9tXJqmEEM6k*^woF@wn4@xipxat.uil5m2u.mongodb.net/xipxat?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
 
 }
