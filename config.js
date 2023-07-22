e26dd255f0e1eae6c8c1577976643bd1b60571d3
xcomi <xaviercomi@gmail.com>
2023-07-22 13:15:48 +0200

    Add new mongoDB Atlas URI to config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-    
-    DB : process.env.MONGODB || 'mongodb://localhost/xipxat',
+
+    DB: process.env.MONGODB || 'mongodb+srv://xaviercomi:9tXJqmEEM6k*^woF@wn4@xipxat.uil5m2u.mongodb.net/?retryWrites=true&w=majority',
     PORT: process.env.PORT || 3000,
-    
+
 }
